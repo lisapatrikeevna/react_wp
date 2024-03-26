b3bdd7f49cc9c80a6da51e9f95442adc8d7c5035
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2024-03-26 10:55:28 +0100

    carousel-block

--- wp-content/plugins/carousel/build/index.asset.php
+++ wp-content/plugins/carousel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8751809607d426144c94');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3179ca7bb77b6d9b9ea1');
